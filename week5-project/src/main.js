342165d69db762ff663497d12a9e603d66c354ed
Austin Landis <austinlandiscode@gmail.com>
2021-02-05 15:43:10 -0800

    set up environment

--- week5-project/src/main.js
+++ week5-project/src/main.js
@@ -1,2 +1,6 @@
 import Planets from './planets.js';
 import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
