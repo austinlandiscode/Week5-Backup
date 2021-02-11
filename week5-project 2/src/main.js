474158d9f3ac4903d57bbee13f337571b0aeca53
Austin Landis <austinlandiscode@gmail.com>
2021-02-10 22:33:44 -0800

    add form

--- week5-project 2/src/main.js
+++ week5-project 2/src/main.js
@@ -1,6 +1,12 @@
-import Person from './planets.js';
+import Calculator from './planets.js';
 import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$("#age").submit(function(event) {
+  event.preventDefault():
+
+  
+})
