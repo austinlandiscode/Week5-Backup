6877998f033cbd623d990ef4958536cbccbd98d4
Austin Landis <austinlandiscode@gmail.com>
2021-02-11 12:06:37 -0800

    condense project files

--- week5-project/src/main.js
+++ week5-project/src/main.js
@@ -1,6 +1,12 @@
-import Person from './planets.js';
+import Calculator from '../../week5-project 2/src/planets.js';
 import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$("#age").submit(function(event) {
+  event.preventDefault():
+
+  
+})
